c0f0989c06ce47e3cab683910b4fbf8a51e87e03
rohitangira <rohitangira@outlook.com>
2018-04-18 17:31:01 +0530

    add more fields to Hr modules

--- hr_jaarvis/models/hr_employee.py
+++ hr_jaarvis/models/hr_employee.py
@@ -12,3 +12,16 @@ class HrEmployee(models.Model):
     universal_account_number = fields.Char(string='UAN(Universal Account Number)')
     pan_id = fields.Char(string = 'Pan No')
     aadhar_id = fields.Char(string = 'Aadhar No')
+    nominee_pf = fields.Char(string= 'Nominee For Pf')
+    internship_confirmation_date = fields.Date('Internship Confirmation Date')
+    probation_completion_date = fields.Date('Probation Completion Date')
+
+    ####Details of last organization
+    previous_company_name = fields.Char('Company name')
+    previous_company_hr_name = fields.Char('Hr name')
+    previous_company_hr_contact = fields.Char('Hr Contact No')
+
+    #total_experience
+    experience_total_years = fields.Integer(string='Years')
+    experience_months = fields.Integer(string='Months')
+
